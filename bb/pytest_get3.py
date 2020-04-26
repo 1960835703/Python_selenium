c900f6e55ecdb2d0d6de11bbca20a1d45ae5efa5
Zhongrj <367353818@qq.com>
2020-04-26 15:20:25 +0800

    第三次

---
+++ bb/pytest_get3.py
@@ -0,0 +1,28 @@
+# -*-coding:UTF-8 -*-
+import json
+
+import requests
+import pytest
+
+@pytest.fixture()
+def juhe_url():
+    url='http://v.juhe.cn/chengyu/query'
+    return url
+class Test_Get_send():
+    
+    def test_send(self,juhe_url,**kwargs):
+        url=juhe_url
+        params={"key":"53ef017dff7cd19238b5b168dc6f537d","word":"天下无双"}
+        result=requests.get(url,params,**kwargs)
+        user_dic=json.loads(result.text)
+#         return user_dic
+        assert user_dic['error_code']==0
+       
+
+# 调用、第三次测试
+# get=Test_Get_send()
+# response=get.test_send('http://v.juhe.cn/chengyu/query')
+# print(response)
+# if response['error_code']==0:
+#     print('成功')
+#     print('成语解释:%s'%response['result']['chengyujs'])
